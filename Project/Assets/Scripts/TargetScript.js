f7e09ce3ab3da760da39414ebb876abaea4fe3f5
supermiu <jeffersonlee2000@gmail.com>
2014-05-23 06:20:18 -0700

    new parameters (no parameters)

--- Project/Assets/Scripts/TargetScript.js
+++ Project/Assets/Scripts/TargetScript.js
@@ -3,28 +3,23 @@ var LeftButton:KeyCode;
 var RightButton:KeyCode;
 var UpButton:KeyCode;
 var DownButton:KeyCode;
-var cam:Camera;
 var speed:float;
-private var height:float;
-private var width:float;
 function Start () {
-	height = 2f * cam.orthographicSize;
-	width = height * cam.aspect;
 	renderer.material.color = Color.magenta;
 	renderer.material.color.a = 0.5f;
 }
 
 function Update () {
-	if((Input.GetKey(LeftButton)) && (transform.position.x > -1 * (width/2))) {
+	if(Input.GetKey(LeftButton)) {
 		transform.Translate(-1 * speed,0,0);
 	}
-	if((Input.GetKey(RightButton)) && (transform.position.x < (width/2))) {
+	if(Input.GetKey(RightButton)) {
 		transform.Translate(speed,0,0);
 	}
-	if((Input.GetKey(DownButton)) && (transform.position.y > -2)) {
+	if(Input.GetKey(DownButton)) {
 		transform.Translate(0,-1 * speed,0);
 	}
-	if((Input.GetKey(UpButton)) && (transform.position.y < (height/2))) {
+	if(Input.GetKey(UpButton)) {
 		transform.Translate(0,speed,0);
 	}
 }
